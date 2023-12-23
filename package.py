16d4bdaa5c91dd0286c3cb6e7e396b93eefa1091
Nadia ESSID <nadia.essid@gmail.com>
2023-12-23 03:52:26 +0100

    refactor: correct config files path

--- package.py
+++ package.py
@@ -21,9 +21,9 @@ def commands():
     env.PYBLISH_PLUGINS_FOLDERS.append('{root}/pyblish_plugins/pyblish_plugins_maya/plugins')
 
     # Path to the JSON file listing pyblish plugins active states by asset_types and tasks.
-    env.PYBLISH_PLUGINS_SETTINGS_BY_TASKS_JSON = '{root}/pyblish_config/pyblish_plugins_settings_by_tasks.json'
+    env.PYBLISH_PLUGINS_SETTINGS_BY_TASKS_JSON = '{root}/pyblish_plugins_manager/config/pyblish_plugins_settings_by_tasks.json'
     # Path to the JSON file listing production assets types and their associated tasks
-    env.PYBLISH_ASSET_TASKS_MAPPING_JSON = '{root}/pyblish_config/asset_tasks_mapping.json'
+    env.PYBLISH_ASSET_TASKS_MAPPING_JSON = '{root}/pyblish_plugins_manager/config/asset_tasks_mapping.json'
 
     env.MAYA_SCRIPT_PATH.append('{root}/pyblish_lite/pythonpath')
     env.PYTHONPATH.append("{env.MAYA_SCRIPT_PATH}")  # Needed for userSetupy.py to execute
