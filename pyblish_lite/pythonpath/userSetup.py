fea4681068732f2bdd4768f812b6dc94c1e16188
Nadia ESSID <nadia.essid@gmail.com>
2023-12-28 10:07:20 +0100

    refactor: delete useless userSetup (since pyblish_core init has been emptied)

--- pyblish_lite/pythonpath/userSetup.py
+++
@@ -1,9 +0,0 @@
-
-try:
-    __import__("pyblish_core")
-
-except ImportError as e:
-    import traceback
-
-    print("pyblish_lite: Could not load integration: %s"
-          % traceback.format_exc())
