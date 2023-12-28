d63f7b2cd9303ab64ab2d29f9b537f5c8e216eac
Nadia ESSID <nadia.essid@gmail.com>
2023-12-28 02:40:51 +0100

    chore: add log

--- pyblish_core/env_lib.py
+++ pyblish_core/env_lib.py
@@ -1,4 +1,7 @@
 import os
+from pyblish_core.lib import configure_logging
+
+log = configure_logging(__file__)
 
 
 def add_path_to_env_var(env_var_name, path_to_add):
@@ -22,5 +25,8 @@ def setup_plugins_folders_env_variable(caller_file):
     :param caller_file: The __file__ attribute of the calling script.
     """
     root = os.path.realpath(os.path.join(os.path.dirname(caller_file)))
+    plugins_path = os.path.join(root, 'plugins')
 
-    add_path_to_env_var('PYBLISH_PLUGINS_FOLDERS', os.path.join(root, 'plugins'))
+    add_path_to_env_var('PYBLISH_PLUGINS_FOLDERS', plugins_path)
+
+    log.info(f'{os.path.basename(os.path.dirname(os.path.realpath(caller_file)))} added to PYBLISH_PLUGINS_FOLDERS')
